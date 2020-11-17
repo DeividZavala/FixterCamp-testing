ac74a667a666369b6f8214cb898d56d09c478041
David <crusedmoss@gmail.com>
2020-11-17 17:39:12 -0600

    format changes

--- semana-2/testing-redux/src/App.js
+++ semana-2/testing-redux/src/App.js
@@ -1,20 +1,20 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Header from "./components/Header";
-import Card from "./components/Card";
-import Cart from "./components/Cart";
-import { getProducts } from "./redux/productDuck";
-import { getCart, editCart } from "./redux/cartDuck";
-import { denormalizeData } from "./utils";
-import "./App.css";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Header from './components/Header';
+import Card from './components/Card';
+import Cart from './components/Cart';
+import { getProducts } from './redux/productDuck';
+import { getCart, editCart } from './redux/cartDuck';
+import { denormalizeData } from './utils';
+import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-  const products = useSelector((state) => denormalizeData(state.products.data));
-  const status = useSelector((state) => state.products.status);
-  const error = useSelector((state) => state.products.error);
-  const cartItems = useSelector((state) => state.cart.data);
-  const items = denormalizeData(cartItems)
+  const products = useSelector(state => denormalizeData(state.products.data));
+  const status = useSelector(state => state.products.status);
+  const error = useSelector(state => state.products.error);
+  const cartItems = useSelector(state => state.cart.data);
+  const items = denormalizeData(cartItems);
 
   useEffect(() => {
     dispatch(getProducts());
@@ -22,52 +22,52 @@ function App() {
   }, [dispatch]);
 
   const handleEdit = (item, type) => {
-    if(item.id in cartItems) {
-      if(type === "delete" || (type === "remove" && cartItems[item.id].quantity - 1 === 0)){
-        const filtered = Object.values(cartItems).filter(i=>item.id !== i.id)
-        const newOrder = {products:filtered}
-        dispatch(editCart(newOrder))
-      }else{
-        const match = {...cartItems[item.id]}
-        const {quantity} = match
-        const newQuantity = type === "add" ? quantity+1 : quantity-1
-        const edited = {...match,  quantity: newQuantity}
-        const editedItems = {...cartItems, [edited.id]: edited}
-        const newOrder = {products:denormalizeData(editedItems)}
-        dispatch(editCart(newOrder))
+    if (item.id in cartItems) {
+      if (type === 'delete' || (type === 'remove' && cartItems[item.id].quantity - 1 === 0)) {
+        const filtered = Object.values(cartItems).filter(i => item.id !== i.id);
+        const newOrder = { products: filtered };
+        dispatch(editCart(newOrder));
+      } else {
+        const match = { ...cartItems[item.id] };
+        const { quantity } = match;
+        const newQuantity = type === 'add' ? quantity + 1 : quantity - 1;
+        const edited = { ...match, quantity: newQuantity };
+        const editedItems = { ...cartItems, [edited.id]: edited };
+        const newOrder = { products: denormalizeData(editedItems) };
+        dispatch(editCart(newOrder));
       }
-    }else{
-      const items = {...cartItems, [item.id]: {...item, quantity:1}}
-      const newOrder = {products:denormalizeData(items)}
-      dispatch(editCart(newOrder))
+    } else {
+      const items = { ...cartItems, [item.id]: { ...item, quantity: 1 } };
+      const newOrder = { products: denormalizeData(items) };
+      dispatch(editCart(newOrder));
     }
-  }
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <Header items={items} />
-      <div className="uk-section">
-        <div className="uk-container uk-container-expand">
-          <div className="uk-grid uk-grid-small uk-grid-match">
-            <div className="uk-width-2-3">
-              {status === "fetching" && <div uk-spinner="ratio: 6"></div>}
-              {status === "finished" && (
-                <div className="uk-grid uk-child-width-1-3 uk-grid-small uk-grid-match">
+      <div className='uk-section'>
+        <div className='uk-container uk-container-expand'>
+          <div className='uk-grid uk-grid-small uk-grid-match'>
+            <div className='uk-width-2-3'>
+              {status === 'fetching' && <div uk-spinner='ratio: 6'></div>}
+              {status === 'finished' && (
+                <div className='uk-grid uk-child-width-1-3 uk-grid-small uk-grid-match'>
                   {products.map((prod, index) => (
                     <Card key={index} {...prod} addItem={handleEdit} />
                   ))}
                 </div>
               )}
-              {status === "error" && (
+              {status === 'error' && (
                 <div
-                  className="uk-alert-danger uk-flex uk-flex-middle uk-flex-center"
-                  uk-alert="true"
+                  className='uk-alert-danger uk-flex uk-flex-middle uk-flex-center'
+                  uk-alert='true'
                 >
                   <p>{error}</p>
                 </div>
               )}
             </div>
-            <div className="uk-width-1-3">
+            <div className='uk-width-1-3'>
               <Cart items={items} editCart={handleEdit} />
             </div>
           </div>
