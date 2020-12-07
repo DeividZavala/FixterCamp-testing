a72176a0314b8ea45d795b258f7ea361c700b29a
David <crusedmoss@gmail.com>
2020-12-07 11:07:12 -0600

    msw setup and tests

--- semana-2/code/testing-msw/src/App.test.js
+++ semana-2/code/testing-msw/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import { server, rest } from "../jest/server";
 
-test('renders learn react link', () => {
+test("<App/> renders and fetch todos", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const items = await waitFor(() => screen.getAllByRole("listitem"));
+  expect(items.length).toBe(1);
+});
+
+test("<App/> renders error", async () => {
+  server.use(
+    rest.get("http://localhost:4000/todos", (req, res, ctx) => {
+      return res(ctx.status(500), ctx.json({ message: "http error" }));
+    })
+  );
+  render(<App />);
+  const error = await waitFor(() => screen.getByRole("heading"));
+  expect(error).toBeInTheDocument();
+  expect(error).toHaveTextContent("http error");
 });
